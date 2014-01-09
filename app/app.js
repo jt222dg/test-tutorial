2cf1c5a2a3f16ca858c7cdaef701dc55ae1876c6
Jesper Tullberg <jt222dg@student.lnu.se>
2014-01-09 15:46:08 -0500

    pics..

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 define(function(require) {
   var Model = require('app/model');
+  var View  = require('app/view');
   
   return {
     model : new Model(),
+    view  : new View(),
     
     save : function() {
       this.model.save();
